b5bdb89a0d2b22e827a0e702d1cf835608a35552
chuntley56 <coreyhuntley562@gmail.com>
2018-09-26 17:46:00 -0700

    init calculator

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './components/calculator.js';
+
+
+
+ReactDOM.render(<Calculator />, document.querySelector('#app'));
